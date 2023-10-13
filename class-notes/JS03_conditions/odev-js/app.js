0a349590024d5df6cf6c0dc18ac8cac9d4449bdf
skrandysn <sakiresolmaz@gmail.com>
2023-10-13 16:30:14 +0300

    js-6

--- class-notes/JS03_conditions/odev-js/app.js
+++ class-notes/JS03_conditions/odev-js/app.js
@@ -1,24 +1,36 @@
 //! ODEV1: Kullanıcıdan 2 sayı ve 1 işlem istenerek basit bir hesap makinesi programını yazınız.
 
-// const s1 = Number(prompt("1.Sayıyı Giriniz:"));
-// const islem = prompt("İşlemi giriniz: +,-,*,/");
-// const s2 = Number(prompt("2.Sayıyı Giriniz:"));
+const s1 = Number(prompt("1.Sayıyı Giriniz:"));
+const islem = prompt("İşlemi giriniz: +,-,*,/");
+const s2 = Number(prompt("2.Sayıyı Giriniz:"));
 
-// if (islem === "+") {
-// 	console.log(`${s1 + s2} toplama işlemi sonucu`);
-// } else if (islem === "-") {
-// 	console.log(`${s1 - s2} çıkarma işlemi sonucu`);
-// } else if (islem === "*") {
-// 	console.log(`${s1 * s2} çarpma işlemi sonucu`);
-// } else if (islem === "/") {
-// 	console.log(`${s1 / s2} bölme işlemi sonucu`);
-// } else {
-// 	console.log("Lütfen seçili işlemlerden bir tanesini seçiniz.");
-// }20
+if (islem === "+") {
+	console.log(`${s1 + s2} toplama işlemi sonucu.`);
+} else if (islem === "-") {
+	console.log(`${s1 - s2} çıkarma işlemi sonucu.`);
+} else if (islem === "*") {
+	console.log(`${s1 * s2} çarpma işlemi sonucu.`);
+} else if (islem === "/") {
+	console.log(`${s1 / s2} bölme işlemi sonucu.`);
+} else {
+	console.log(
+		"Hatalı İşlem. Lütfen seçili işlemlerden bir tanesini seçerek tekrar deneyiniz."
+	);
+}
+20;
 
 //! ODEV2:Maasi asgari ucretten az ve eşit olanlara %50 zam,
 //! fazla olanlara ise %10 zam yapmak istiyoruz.
 
+//todo ************* BİRİNCİ ÇÖZÜM ********************
+const maas = Number(prompt("Lütfen Maaşınızı Giriniz:"));
+const asgari = 5000;
+
+maas <= asgari
+	? alert(`Güncel maaşınız %50 zamdan sonra ${maas * 1.5} olacaktır.`)
+	: alert(`Güncel maaşınız %10 zamdan sonra ${maas * 1.1} olacaktır.`);
+
+//todo ************* İKİNCİ ÇÖZÜM ********************
 const maas = Number(prompt("Lütfen Maaşınızı Giriniz:"));
 const asgari = 5000;
 
---
+++ class-notes/JS06/odev/index.js
@@ -0,0 +1,63 @@
+//? ********** ODEV ***********
+
+//! ODEV-1 taban ve yükseklik değerlerini parametre olarak alan ve bir üçgenin alanını hesaplayarak ana programa döndüren fonksiyonu yazınız.
+
+const taban = Number(
+	prompt(
+		"Lütfen alanını hesaplamak istediğiniz üçgenin TABAN KENAR UZUNLUĞUNU cm değerinden giriniz."
+	)
+);
+const yukseklik = Number(
+	prompt(
+		"Lütfen alanını hesaplamak istediğiniz üçgenin yüksekliğini cm değerinden giriniz."
+	)
+);
+let alan = 1;
+
+function alanHesaplama(taban, yukseklik) {
+	return (alan = (taban * yukseklik) / 2);
+}
+alanHesaplama(taban, yukseklik);
+console.log(`Hesapladığınız üçgenin alanı: ${alan}`);
+
+//! ODEV-2 "kareAl" "küpAl" "üsAl"şeklinde üç adet farklı declaration veya expresssion(dec-ex ile yapıcaz) tanımlayın. Bu fonksiyonların ana programdan gereken parametreleri alarak sonuçları ana programa döndürmeli gerekmektedir.
+
+//*kareAl**
+const sayi = Number(
+	prompt("Lütfen KARESİNİ almak istediğiniz sayıyı giriniz.")
+);
+
+let kareAl = 0;
+
+function kareHesaplama() {
+	return (kareAl = sayi ** 2);
+}
+
+kareHesaplama();
+console.log(`Hesapladığınız sayının karesi: ${kareAl}`);
+
+//*** kupAl */
+const sayi = Number(prompt("Lütfen KÜPÜNÜ almak istediğiniz sayıyı giriniz."));
+
+let kupAl = 0;
+
+function kupHesaplama() {
+	return (kupAl = sayi ** 3);
+}
+
+kupHesaplama();
+console.log(`Hesapladığınız sayının küpü: ${kupAl}`);
+
+//*üsAl***
+
+const sayi = Number(prompt("Lütfen  sayıyı giriniz."));
+const sayiUssu = Number(prompt("Lütfen ÜSSÜNÜ  giriniz."));
+
+let usAl = 0;
+
+function usHesaplama() {
+	return (usAl = sayi ** sayiUssu);
+}
+
+usHesaplama();
+console.log(`Hesapladığınız sayının üssü: ${usAl}`);
